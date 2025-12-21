b4a64bb6f90aa8b2715eda0aae5f8572a3a08d2b
dhanajit96 <ajitdhanje@gmail.com>
2025-12-21 22:50:37 +0530

    added the changes

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -4,15 +4,40 @@ class SchoolsController < ApplicationController
 
   def index
     @page_title = "Manage Schools"
-    @breadcrumb_list = [["Home", root_path], ["Schools", nil]]
+    @breadcrumb_list = [ [ "Home", root_path ], [ "Schools", nil ] ]
     # The 'nil' url makes it the active/last item in breadcrumb logic
-    
-    @actions = [["Add School", new_school_path]]
+
+    @actions = [ [ "Add School", new_school_path ] ]
+  end
+
+  def show
+    # @school is already loaded by load_and_authorize_resource
+    #
+    @school_admins = @school.users.where(role: :school_admin)
+
+    @page_title = @school.name
+    @breadcrumb_list = [ [ "Home", root_path ], [ "Schools", schools_path ], [ @school.name, nil ] ]
+
+    # Action buttons for Admin
+    @actions = []
+    if can? :update, @school
+      @actions << [ "Edit School", edit_school_path(@school) ]
+    end
+
+    # Data Fetching
+    # 1. Students belonging to this school
+    @students = @school.users.where(role: :student).order(created_at: :desc).page(params[:student_page]).per(5)
+
+    # 2. Courses belonging to this school
+    @courses = @school.courses.includes(:batches)
+
+    # 3. Recent/Active Batches (Through courses)
+    @batches = @school.batches.includes(:course).order(start_date: :desc).page(params[:batch_page]).per(5)
   end
 
   def new
     @page_title = "Create New School"
-    @breadcrumb_list = [["Home", root_path], ["Schools", schools_path], ["New", nil]]
+    @breadcrumb_list = [ [ "Home", root_path ], [ "Schools", schools_path ], [ "New", nil ] ]
     @actions = []
   end
 
@@ -21,14 +46,14 @@ class SchoolsController < ApplicationController
       redirect_to schools_path, notice: "School created successfully."
     else
       @page_title = "Create New School"
-      @breadcrumb_list = [["Home", root_path], ["Schools", schools_path], ["New", nil]]
+      @breadcrumb_list = [ [ "Home", root_path ], [ "Schools", schools_path ], [ "New", nil ] ]
       render :new
     end
   end
 
   def edit
     @page_title = "Edit School"
-    @breadcrumb_list = [["Home", root_path], ["Schools", schools_path], [@school.name, nil]]
+    @breadcrumb_list = [ [ "Home", root_path ], [ "Schools", schools_path ], [ @school.name, nil ] ]
     @actions = []
   end
 
